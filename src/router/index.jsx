73713ebab162d4fed13534ca7871ffd45db7913a
nyeinhsuthwe <nyeinhsuthwe57@gmail.com>
2024-02-02 16:05:42 +0630

    course page

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -4,6 +4,9 @@ import {
 import Home from '../pages/user/Home'
 import Layout from "../pages/user/layouts/Layout";
 import About from "../pages/user/About"
+import Course from "../pages/user/Course";
+import Course1 from "../pages/user/Course1";
+import Course2 from "../pages/user/Course2";
 
 const router = createBrowserRouter([
     {
@@ -11,12 +14,24 @@ const router = createBrowserRouter([
         element: <Layout />,
         children: [
             {
-                path: "",
+                path: "/home",
                 element: <Home/>
             },
             {
                 path: "/about",
                 element: <About/>
+            },
+            {
+                path: "/course",
+                element: <Course/>
+            },
+            {
+                path: "/course1",
+                element: <Course1/>
+            },
+            {
+                path: "/course2",
+                element: <Course2/>
             }
         ]
     },
