10d28a78fbe4fbc02d0bb19ed4cd896013d10892
saithihan <saithihan668@gmail.com>
2024-02-03 00:02:20 +0630

    develop dashboard

--- src/pages/admin/AdminDashboard.jsx
+++ src/pages/admin/AdminDashboard.jsx
@@ -1,7 +1,91 @@
 export default function AdminDashboard() {
   return (
-    <div>
-      <h2 className="text-2xl font-semibold mb-4">Dashboard</h2>
+    <div className="container mx-auto mt-4 px-4 sm:px-6 md:px-8">
+      <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto">
+          <div className="py-3 pl-2">
+            <div className="relative max-w-xs">
+              <div className="flex items-center">
+                <input
+                  type="text"
+                  name="hs-table-search"
+                  id="hs-table-search"
+                  className="block w-full p-3 pl-12 text-sm border-gray-200 rounded-md focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400"
+                  placeholder="Search..."
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="p-1.5 w-full inline-block align-middle">
+            <div className="overflow-hidden border rounded-lg">
+              <table className="min-w-full divide-y divide-gray-200">
+                <thead className="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                    >
+                      ID
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-xs font-bold text-left text-gray-500 uppercase "
+                    >
+                      Email
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                    >
+                      Edit
+                    </th>
+                    <th
+                      scope="col"
+                      className="px-6 py-3 text-xs font-bold text-right text-gray-500 uppercase "
+                    >
+                      Delete
+                    </th>
+                  </tr>
+                </thead>
+                <tbody className="divide-y divide-gray-200">
+                  <tr>
+                    <td className="px-6 py-4 text-sm font-medium text-gray-800 whitespace-nowrap">
+                      1
+                    </td>
+                    <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                      Jone Doe
+                    </td>
+                    <td className="px-6 py-4 text-sm text-gray-800 whitespace-nowrap">
+                      jonne62@gmail.com
+                    </td>
+                    <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                      <a
+                        className="text-green-500 hover:text-green-700"
+                        href="#"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                    <td className="px-6 py-4 text-sm font-medium text-right whitespace-nowrap">
+                      <a className="text-red-500 hover:text-red-700" href="#">
+                        Delete
+                      </a>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
